7f0f4d8d84125e94d3eee083fe021f8ede795ec0
unknown <engtaha1995@outlook.com>
2024-05-12 23:08:19 +0300

    okay ! enough for today

--- DEPI AdventureWorks2019 DB/Assignment 1.sql
+++ DEPI AdventureWorks2019 DB/Assignment 1.sql
@@ -2,7 +2,7 @@
 select name from Production.Product;
 
 --2 Find all employees who have a job title of "Sales Representative".
-select * from Sales.SalesOrderHeader order by OrderDate desc;
+select * from HumanResources.Employee where JobTitle = 'Sales Representative'
 
 --3 Get all orders from the "SalesOrderHeader" table, sorted by order date in descending order.
 select * from sales.SalesOrderHeader order by OrderDate desc;
@@ -16,3 +16,7 @@ ON ProductID is not NULL;
 --4 in simpler solution
 select Product.Name , ProductCategory.Name as Category
 from Production.Product , Production.ProductCategory;
+
+--5 List the total number of products in each product category.
+select Product.Name , ProductCategoryID from Production.Product , Production.ProductCategory
+group by ProductCategoryID -- okay, this may not be correct! i will take a break
