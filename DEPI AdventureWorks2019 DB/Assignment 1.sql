7541085e6f7f4a6485a65af73a24785e4086b9f1
unknown <engtaha1995@outlook.com>
2024-05-16 13:14:20 +0300

    OK

--- DEPI AdventureWorks2019 DB/Assignment 1.sql
+++ DEPI AdventureWorks2019 DB/Assignment 1.sql
@@ -1,6 +1,3 @@
--- always make sure iam connected to the target database
-use AdventureWorks2019;
-
 --1 List all the products in the "Product" table.
 select name from Production.Product;
 
@@ -21,27 +18,44 @@ select Product.Name , ProductCategory.Name as Category
 from Production.Product , Production.ProductCategory;
 
 --5 List the total number of products in each product category.
-select Product.Name , ProductCategoryID from Production.Product , Production.ProductCategory
-group by ProductCategoryID -- okay, this may not be correct! i will take a break
+select  Name from Production.Product  
+join Production.ProductCategory
+on Product = ProductCategoryID
+;
 
---6 
-select * from AdventureWorks2019.Production.Product order by Name asc offset 10 FETCH 5;
+--6 Retrieve the next 5 products after skipping the first 10 products, sorted by product name in ascending order.
+select * from Production.Product 
+order by Name asc 
+offset 10 rows 
+FETCH next 5 row only
+
+--7 List all possible combinations of product names and sales territory names
+select Name  , saleName
+from Production.Product , Sales.SalesTerritory
+cross join
+;
+
 
 --8 Find all orders with a total due between $50000 and $100000
-select * from AdventureWorks2019.Sales.SalesOrderDetail where UnitPrice between 50000 and 100000
+select * from Sales.SalesOrderDetail where UnitPrice between 50000 and 100000
 
 --9 Find all products whose names start with 'Bike'
-select Name from AdventureWorks2019.Production.Product where Name like 'Bike%'
+select Name from Production.Product where Name like 'Bike%'
 
 --10- Find products with a list price less than $100
-select ProductID, Name, ListPrice from AdventureWorks2019.Production.Product where ListPrice < 100;
+select ProductID, Name, ListPrice from Production.Product where ListPrice < 100 order by ListPrice;
 
 --11 Retrieve all sales orders along with the salesperson's name, even if
 --some orders were not handled by a salesperson
 select name, sale
 
 --12 Retrieve the top 5 most expensive products sorted by list price in descending order
-select top 5  Name, Color, ProductNumber, ListPrice from AdventureWorks2019.Production.Product order by ListPrice desc
+select top 5  Name, Color, ProductNumber, ListPrice from Production.Product order by ListPrice desc
 
 --13 Calculate total sales, average sales, the maximum and minimum sales order amounts, 
 --and the total number of sales orders from the "SalesOrderHeader" table
+select SUM(SalesOrderNumber) as sumofsale , AVG(SalesOrderNumber) as avgS , MAX(SalesOrderNumber) as maxS , MIN(SalesOrderNumber) as MinS from Sales.SalesOrderHeader
+
+--14- Determine the average line total amount for all the items sold in each product category
+
+
